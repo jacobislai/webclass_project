dd3ca3288cef0dff5debc1697e0f758151a8b3bd
jacob <laidesign1006@gmail.com>
2024-06-24 20:19:27 +0800

    文字

---
+++ 20240624/assets/js/js_type.js
@@ -0,0 +1,9 @@
+let firstName = 'Lai';
+let lastName = 'Jacob';
+let fullName = lastName + ' ' + firstName;
+//let fullName = 'lastName' + ' ' + firstName;錯誤
+console.log(fullName);
+
+let t1 = "I'm \"Jacob\"";
+console.log(t1); //I'm "Jacob"
+//"\"為跳脫字元，將有特殊涵意轉為一般文字
