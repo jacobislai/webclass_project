56d1637665bf33992df80bdbcbdf4606414369be
jacob <laidesign1006@gmail.com>
2024-06-24 19:59:21 +0800

    js 變數宣告

---
+++ 20240624/assets/js/js_variable.js
@@ -0,0 +1,12 @@
+//一般輸出
+let fname = 'Jacob';
+console.log(fname);
+console.table(fname);
+
+//列表輸出
+let nums = [1, 2, 3, 4, 5]
+console.table(nums);
+console.log(nums);
+
+//特殊格式輸出
+console.log('')
