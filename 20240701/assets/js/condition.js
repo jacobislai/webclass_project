ff9c9c49373e9b46994044204a4b207ef9e01210
jacob <laidesign1006@gmail.com>
2024-07-01 19:43:14 +0800

    相等運算子

---
+++ 20240701/assets/js/condition.js
@@ -0,0 +1,43 @@
+let n1 = 10;
+let n2 = 100;
+
+// console.log(`n1: ${n1}, n2: ${n2}`);
+
+// if (n1 == n2) {
+//     console.log('equal!');
+// } else {
+//     console.log('diff');
+// }
+
+
+
+// // n1 = n2 => n1 = 100 => if (100)
+// if (n1 = n2) {
+//     console.log('equal!');
+// } else {
+//     console.log('diff');
+// }
+
+// console.log(`n1: ${n1}, n2: ${n2}`);
+
+
+let m1 = 10;
+let m2 = '10';
+
+console.log(`m1: ${m1} type is ${typeof m1}, m2: ${m2} type is ${typeof m2}`)
+
+console.log('use == is: ');
+
+if (m1 == m2) {
+    console.log('equal');
+} else {
+    console.log('diff');
+}
+
+console.log('use === is: ');
+
+if (m1 === m2) {
+    console.log('equal');
+} else {
+    console.log('diff');
+}
