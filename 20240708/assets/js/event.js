6ecc031330e877effb1b5c55b7ef682c4c2e253e
jacob <laidesign1006@gmail.com>
2024-07-08 20:46:56 +0800

    用keyup完成過濾

--- 20240708/assets/js/event.js
+++ 20240708/assets/js/event.js
@@ -15,17 +15,21 @@ cLink.addEventListener(clickType, clickCallback)
 
 let chAccount = document.querySelector('#ch-account');
 
-chAccount.addEventListener('change', () => {
-    let value = chAccount.value;
-    console.log(value.length);
-    if (value.length > 8) {
-        alert('超過8個了喔!')
-        value = value.substr(0, 8);
-        chAccount.value = value
-    }
-    console.log(`trigger change value is: ${chAccount.value}`)
-})
+
 
 chAccount.addEventListener('blur', () => {
     console.log(`trigger blur value is: ${chAccount.value}`)
+})
+
+chAccount.addEventListener('change', () => {
+    chAccount.value = chAccount.value.substr(0, 8);
+})
+
+chAccount.addEventListener('keypress', () => {
+    console.log(`trigger keypress value is: ${chAccount.value}`)
+})
+
+chAccount.addEventListener('keyup', () => {
+    console.log(`trigger keyup value is: ${chAccount.value}`)
+    chAccount.value = chAccount.value.substr(0, 8);
 })
