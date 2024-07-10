5ba0c0cb54340c2aa8f0ff29d503135ef13c8bf8
jacob <laidesign1006@gmail.com>
2024-07-10 19:40:34 +0800

    事件中斷

--- 20240710/assets/js/event_bubble.js
+++ 20240710/assets/js/event_bubble.js
@@ -1,10 +1,15 @@
 let addBtn = document.querySelector('#add-btn');
 let ul = document.querySelector('#menu');
 
+document.addEventListener('click', (e) => {
+    console.log('document click', e)
+})
+
 ul.addEventListener('click', (e) => {
+    e.stopPropagation();
     let target = e.target;
-    let html = target.innerHTML;
-    let tag = target.tagName;
+    let html = target.innerHTML
+    let tag = target.tagName
     if (tag.toUpperCase() == 'LI') {
         console.log(html, tag);
     }
@@ -21,7 +26,6 @@ ul.addEventListener('click', (e) => {
 // }
 
 addBtn.addEventListener('click', () => {
-    let ul = document.querySelector('#menu');
     let li = document.createElement('li');
     li.innerHTML = 'ADD';
     ul.appendChild(li);
@@ -30,10 +34,19 @@ addBtn.addEventListener('click', () => {
     // 1.會有重複綁定
     // bindLiClick();
 
-    // 2.會有效能瓶頸
+    // 2. 會有效能瓶頸
     // ul.innerHTML += '<li>ADD</li>'
     // bindLiClick();
 
 })
 
 // bindLiClick();
+
+let openMenu = document.querySelector('#open-menu');
+
+openMenu.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('open menu!');
+    console.log(openMenu.href);
+    location.href = openMenu.href;
+})
