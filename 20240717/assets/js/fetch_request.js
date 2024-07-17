52c6c0c80bb9e06dff6f4c3d47cef7bc9445b1d5
jacob <laidesign1006@gmail.com>
2024-07-17 20:23:27 +0800

    fetch POST

--- 20240717/assets/js/fetch_request.js
+++ 20240717/assets/js/fetch_request.js
@@ -1,12 +1,35 @@
 let domSend = document.querySelector('#send');
 let domResponse = document.querySelector('#response');
 
+const doGet = async (url) => {
+    let response = await fetch(url);
+    return await response.json();
+}
+
+const doPost = async (url) => {
+    let params = {
+        action: 'demo',
+        data: { a: 1, b: 2 }
+    };
+
+    let options = {
+        method: 'POST',
+        body: JSON.stringify(params)
+    };
+
+    let response = await fetch(url, options);
+    return await response.json();
+}
+
 domSend.addEventListener('click', async () => {
     console.log('send!');
-    let response = await fetch('https://book.niceinfos.com/frontend/api/?action=sleep&timer=3');
-    let data = await response.text();
-    data = JSON.parse(data);
-    // let data = await response.json();
-    console.log(data);
-    domResponse.innerHTML = data.data;
+    // let url = 'https://book.niceinfos.com/frontend/api/?action=sleep&timer=3';
+    // let response = await doGet(url);
+    // console.log(response);
+    // domResponse.innerHTML = response.data;
+
+    let url = 'https://book.niceinfos.com/frontend/api/'
+    let response = await doPost(url);
+    console.log(response);
+    domResponse.innerHTML = JSON.stringify(response.data);
 })
